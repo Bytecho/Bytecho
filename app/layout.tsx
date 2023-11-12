a1d76c67bfc608eb0d4c2a9cad6d205606db83bc
Juan <sernadominguezj@gmail.com>
2023-11-12 12:00:06 +1100

    App done according to design for sm devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 
 // Components
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
